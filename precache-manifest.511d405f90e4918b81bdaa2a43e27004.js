2c685a6a8a3e49515b204a3d21837e65ffb42832
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2020-09-25 12:36:43 +0530

    Updates

---
+++ precache-manifest.511d405f90e4918b81bdaa2a43e27004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e420de368e36e7cc820522c894ad54d",
+    "url": "/CodeIO-IDE/index.html"
+  },
+  {
+    "revision": "0cb5ef14d4c6592d53b2",
+    "url": "/CodeIO-IDE/static/css/2.b678d0ad.chunk.css"
+  },
+  {
+    "revision": "dadddc5a9cdb153294f2",
+    "url": "/CodeIO-IDE/static/css/main.7fad573a.chunk.css"
+  },
+  {
+    "revision": "0cb5ef14d4c6592d53b2",
+    "url": "/CodeIO-IDE/static/js/2.c6430027.chunk.js"
+  },
+  {
+    "revision": "f70ed24c7e3911447d378b22f62dae26",
+    "url": "/CodeIO-IDE/static/js/2.c6430027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dadddc5a9cdb153294f2",
+    "url": "/CodeIO-IDE/static/js/main.da6eab94.chunk.js"
+  },
+  {
+    "revision": "8a2b91c8676b94dc7adc",
+    "url": "/CodeIO-IDE/static/js/runtime-main.5636248e.js"
+  }
+]);
