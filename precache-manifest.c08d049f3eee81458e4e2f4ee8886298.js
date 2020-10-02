26c1d2bcd07e99271bebc6cee1b89ceb0d801c7f
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2020-10-02 20:49:03 +0530

    Updates

---
+++ precache-manifest.c08d049f3eee81458e4e2f4ee8886298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "503c4ee7d562cd1075f6ce1ff94662f1",
+    "url": "/CodeIO-IDE/index.html"
+  },
+  {
+    "revision": "28c1ca187decf31aed68",
+    "url": "/CodeIO-IDE/static/css/2.995ce909.chunk.css"
+  },
+  {
+    "revision": "06cab689b5ed24404656",
+    "url": "/CodeIO-IDE/static/css/main.50e65fac.chunk.css"
+  },
+  {
+    "revision": "28c1ca187decf31aed68",
+    "url": "/CodeIO-IDE/static/js/2.2f46c70d.chunk.js"
+  },
+  {
+    "revision": "f70ed24c7e3911447d378b22f62dae26",
+    "url": "/CodeIO-IDE/static/js/2.2f46c70d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cab689b5ed24404656",
+    "url": "/CodeIO-IDE/static/js/main.09ba8f50.chunk.js"
+  },
+  {
+    "revision": "8a2b91c8676b94dc7adc",
+    "url": "/CodeIO-IDE/static/js/runtime-main.5636248e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CodeIO-IDE/precache-manifest.8346b34a523c09a349253aaf29e54a00.js"
+  "/CodeIO-IDE/precache-manifest.c08d049f3eee81458e4e2f4ee8886298.js"
 );
 
 self.addEventListener('message', (event) => {
